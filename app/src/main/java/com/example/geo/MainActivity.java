f12bef33d2ab8de6580fce7b92e367e717e4e435
Eduardoodanniel <83083328+Eduardoodanniel@users.noreply.github.com>
2021-05-06 10:48:26 -0500

    login-val

--- app/src/main/java/com/example/geo/MainActivity.java
+++ app/src/main/java/com/example/geo/MainActivity.java
@@ -3,12 +3,50 @@ package com.example.geo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText usuario,contraseña;
+    Button iniciar_sesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usuario = (EditText) findViewById(R.id.txi_usu);
+        contraseña = (EditText) findViewById(R.id.txi_pass);
+    }
+
+    public void agregar(View V){
+
+        if (validar()){
+            Toast.makeText(this, "Ingreso datos", Toast.LENGTH_SHORT).show();
+
+
+        }
+    }
+
+
+    public boolean validar (){
+
+        boolean retorno = true;
+
+        String c1= usuario.getText().toString();
+        String c2= contraseña.getText().toString();
+
+        if (c1.isEmpty()){
+            usuario.setError("Introduzca su usuario");
+            retorno= false;
+        }
+        if (c2.isEmpty()){
+            contraseña.setError("Introduzca su contraseña");
+            retorno= false;
+        }
+
+        return true;
     }
 }
